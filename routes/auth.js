ea4caa86a7151149535539729419423ab6d6b8f0
veenaa014 <47256853+veenaa014@users.noreply.github.com>
2020-09-14 19:38:58 -0600

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {signup, signin, signout, requireSignin} = require("../controllers/auth");
+const {userById} = require("../controllers/user");
+const {userSignupValidator} = require("../validator");
+const router = express.Router();
+
+router.post("/signup",userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+//any route containing userId, our app will first execute userById()
+router.param("userId", userById);
+
+module.exports = router;
